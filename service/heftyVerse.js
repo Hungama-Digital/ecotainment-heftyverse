cfc19137ce7cd1bcc0c2a1d57461edffdb147279
BrijHungama <brij.mishra@hungama.com>
2024-06-27 17:55:35 +0530

    formate code

--- service/heftyVerse.js
+++ service/heftyVerse.js
@@ -18,7 +18,7 @@ class HeftyVerse {
       // Fetch all transaction IDs in one query
       const searchQuery = `SELECT transaction_id FROM ${process.env.MSDATABASE}.dumpexceldata`;
       const transactionIds = await this.queryPromise(searchQuery);
-  
+
       if (transactionIds.length === 0) {
         console.log("Transaction Id Not Found!");
         return {
@@ -26,7 +26,7 @@ class HeftyVerse {
           error: "Transaction Id Not Found!",
         };
       }
-  
+
       for (const transactionId of transactionIds) {
         const detailQuery = `
           SELECT d.buyer_email, d.buyer_name, d.buyer_phone, d.original_cost, 
@@ -35,35 +35,38 @@ class HeftyVerse {
           WHERE d.transaction_id = '${transactionId.transaction_id}'
           GROUP BY d.transaction_id, d.buyer_email, d.buyer_phone, d.buyer_name, d.original_cost;
         `;
-  
+
         try {
           const HeftyData = await this.queryPromise(detailQuery);
           if (HeftyData.length > 0) {
-            HeftyData[0].buyer_phone = parseInt(HeftyData[0].buyer_phone);
-            HeftyData[0].original_cost = HeftyData[0].original_cost.toString();
-            HeftyData[0].ticket_details = HeftyData[0].ticket_details.slice(1, -1);
-            let ticketArray = HeftyData[0].ticket_details.split(',');
-            HeftyData[0].ticket_details = ticketArray.map(ticket => ({"ticket_id": ticket.trim().replace(/"/g, '')}));
-  
+            let data = HeftyData[0];
+            data.buyer_phone = parseInt(data.buyer_phone);
+            data.original_cost = data.original_cost.toString();
+            data.ticket_details = data.ticket_details.slice(1, -1);
+            let ticketArray = data.ticket_details.split(",");
+            data.ticket_details = ticketArray.map((ticket) => ({
+              ticket_id: ticket.trim().replace(/"/g, ""),
+            }));
+
             try {
               const userId = await this.generateUniqueId();
               console.log("userId", userId);
-  
+
               let ticketDetails = "";
-              HeftyData[0].ticket_details.forEach((ele) => {
-                ticketDetails = ticketDetails + "," + ele;
+              data.ticket_details.forEach((ele) => {
+                ticketDetails = ticketDetails + "," + JSON.stringify(ele);
               });
-              // Replace the first comma with an empty string
-              ticketDetails = ticketDetails.replace(/,/, "");
+              ticketDetails = ticketDetails.replace(/^,/, "");
+
               const insertQuery = `INSERT INTO ${process.env.MSDATABASE}.ticketinfo 
                       (id, buyer_email, buyer_phone, buyer_name, original_cost, ticket_details) 
                       VALUES (?, ?, ?, ?, ?, ?)`;
               const values = [
                 userId,
-                HeftyData[0].buyer_email,
-                HeftyData[0].buyer_phone,
-                HeftyData[0].buyer_name,
-                HeftyData[0].original_cost,
+                data.buyer_email,
+                data.buyer_phone,
+                data.buyer_name,
+                data.original_cost,
                 ticketDetails,
               ];
               await this.queryPromise(insertQuery, values);
@@ -72,21 +75,31 @@ class HeftyVerse {
               console.error("Error dumping data to DB:", error);
             }
 
-            // try {
-            //   const heftyResponse = await this.heftyCall(HeftyData[0]);
-            //   console.log(`Hefty Call Success for transaction ${transactionId.transaction_id}`, heftyResponse);
-            // } catch (heftyError) {
-            //   console.error(`Hefty Call Error for transaction ${transactionId.transaction_id}:`, heftyError);
-            // }
-
+            try {
+              const heftyResponse = await this.heftyCall(data);
+              console.log(
+                `Hefty Call Success for transaction ${transactionId.transaction_id}`,
+                heftyResponse
+              );
+            } catch (heftyError) {
+              console.error(
+                `Hefty Call Error for transaction ${transactionId.transaction_id}:`,
+                heftyError
+              );
+            }
           } else {
-            console.error(`No data found for transaction ${transactionId.transaction_id}`);
+            console.error(
+              `No data found for transaction ${transactionId.transaction_id}`
+            );
           }
         } catch (error) {
-          console.error(`Error processing transaction ${transactionId.transaction_id}:`, error);
+          console.error(
+            `Error processing transaction ${transactionId.transaction_id}:`,
+            error
+          );
         }
       }
-  
+
       return {
         message: "Successfully Sent Data To Hefty Verse",
         statusCode: 200,
@@ -99,7 +112,6 @@ class HeftyVerse {
       };
     }
   };
-  
   // -------------------------------------
 
   HeftyVerseDataInDb = (payload) => {
